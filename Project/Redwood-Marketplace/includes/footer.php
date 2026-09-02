9ea5626cce3230c2763ccbadec4b26aef355f619
Mahi <shaharyarmahi@gmail.com>
2026-09-02 17:23:13 +0600

    feat: add common header and footer

---
+++ Project/Redwood-Marketplace/includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="site-footer">
+    <p>
+        &copy; 2026 Redwood Marketplace. All rights reserved.
+    </p>
+</footer>
+
+</body>
+
+</html>
---
+++ Project/Redwood-Marketplace/includes/header.php
@@ -0,0 +1,23 @@
+<?php
+$baseUrl = "/WT_Summer_2025-26/Project/Redwood-Marketplace";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+
+    <title>
+        <?php echo $pageTitle ?? "Redwood Marketplace"; ?>
+    </title>
+
+    <link rel="stylesheet" href="<?php echo $baseUrl; ?>/assets/css/style.css">
+
+</head>
+
+<body>
